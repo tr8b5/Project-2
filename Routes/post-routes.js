02f3dac0e584853f5a3c88d9f7828060a9887fdb
mya todd <mya.todd428376@gmail.com>
2021-01-30 16:17:26 -0500

    added a route for getting all the posts

--- Routes/post-routes.js
+++ Routes/post-routes.js
@@ -3,3 +3,12 @@ var path = require("path");
 
 var db = require(".../models");
 
+
+module.exports = function(app) {
+    app.get("/api/posts", function(req, res) {
+        var query = {};
+        if (req.query.clubSocial_db) {
+          query.clubSocial_db = req.query.clubSocial_db;
+        
+
+}
