4152d3a08925affa485996f2a75629743e5490e0
mya todd <mya.todd428376@gmail.com>
2021-01-30 16:27:53 -0500

    included a property to the options

--- Routes/post-routes.js
+++ Routes/post-routes.js
@@ -5,10 +5,27 @@ var db = require(".../models");
 
 
 module.exports = function(app) {
+    //  A route for all the posts
     app.get("/api/posts", function(req, res) {
         var query = {};
         if (req.query.clubSocial_db) {
           query.clubSocial_db = req.query.clubSocial_db;
         
+        }
+
+    
+db.Post.findAll({
+    where: query,
+    include: [db.clubSocial_db]
+  }).then(function(dbPost) {
+    res.json(dbPost);
+  });
+});
+
+
+
+
+
+
 
 }
