6bf1cc4ada177abb699cd3b5a6cbfa99d877a103
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-01-30 13:20:46 -0500

    created server.js code and imported necessary packages

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS project2starter;
--- Creates the "todolist" database --
-CREATE DATABASE project2starter;
+-- Drops the clubSocial if it exists currently --
+DROP DATABASE IF EXISTS clubSocial_db;
+-- Creates the "clubSocial" database --
+CREATE DATABASE clubSocial_db;
--- server.js
+++ server.js
@@ -1,46 +1,17 @@
+//Require express
 const express = require("express");
-const exphbs = require("express-handlebars");
-const handlebars = require("handlebars");
-const {
-  allowInsecurePrototypeAccess,
-} = require("@handlebars/allow-prototype-access");
-const app = express();
-const db = require("./models");
-const TrainsController = require("./controllers/trainsController");
 
+//Create an instance of express
+const app = express();
+
+//Create a PORT
 const PORT = process.env.PORT || 8080;
 
+//Add middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main",
-    handlebars: allowInsecurePrototypeAccess(handlebars),
-  })
-);
-app.set("view engine", "handlebars");
-
-app.get("/", (req, res) => {
-  res.render("index");
+//Listen on the PORT
+app.listen(PORT, function () {
+  console.log(`App listening on PORT: http://localhost:${PORT}`);
 });
-
-app.get("/api/config", (req, res) => {
-  res.json({
-    success: true,
-  });
-});
-
-app.use(TrainsController);
-
-db.sequelize
-  .sync()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
