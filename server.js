5d49f63be85f5b74b908252c37f392ee5e8ee68c
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-01-31 11:43:39 -0500

    finished backend api routes

--- server.js
+++ server.js
@@ -14,60 +14,16 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Require API routes
-app.get("/api/posts", (req, res) => {
-  db.Post.findAll().then((allPosts) => {
-    console.log(allPosts);
-    res.json(allPosts);
-  });
-});
+//Require controllers
+const postController = require("./controllers/postController");
 
-app.get("/api/posts/:id", (req, res) => {
-  db.Post.findOne({ where: { id: req.params.id } }).then((foundPost) => {
-    console.log(foundPost);
-    res.json(foundPost);
-  });
-});
-
-app.post("/api/posts", (req, res) => {
-  db.Post.create({
-    school: req.body.school,
-    subject: req.body.subject,
-    post: req.body.post,
-    votes: req.body.votes,
-  }).then((newPost) => {
-    console.log(newPost);
-    res.json(newPost);
-  });
-});
-
-app.put("/api/posts/:id", (req, res) => {
-  db.Post.update({
-    school: req.body.school,
-    subject: req.body.subject,
-    post: req.body.post,
-    votes: req.body.votes,
-  }).then((updatedPost) => {
-    console.log(updatedPost);
-    res.json(updatedPost);
-  });
-});
-
-app.delete("/api/posts/:id", (req, res) => {
-  db.Post.destroy({
-    where: {
-      id: req.params.id,
-    },
-  }).then(() => {
-    console.log("Post deleted.");
-    res.end();
-  });
-});
+//Use controllers
+app.use(postController);
 
 //Require Views routes
 const viewsRoutes = require("./");
 
 //Listen on the PORT
-app.listen(PORT, function () {
-  console.log(`App listening on PORT: http://localhost:${PORT}`);
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
