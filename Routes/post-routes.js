7b3e8c742664ebe2ab42cf6f55a148d0a3315808
mya todd <mya.todd428376@gmail.com>
2021-01-31 12:16:09 -0500

    added a note in post routes

--- Routes/post-routes.js
+++ Routes/post-routes.js
@@ -3,68 +3,50 @@ var path = require("path");
 
 var db = require(".../models");
 
+module.exports = function (app) {
+  //  A route for all the posts
+  app.get("/api/posts", function (req, res) {
+    var query = {};
+    if (req.query.clubSocial_id) {
+      query.clubSocialID = req.query.clubSocial_id;
+    }
 
-module.exports = function(app) {
-    //  A route for all the posts
-    app.get("/api/posts", function(req, res) {
-        var query = {};
-        if (req.query.clubSocial_id) {
-          query.clubSocialID = req.query.clubSocial_id;
-        
-        }
-
-    
-db.Post.findAll({
-    where: query,
-    include: [db.clubSocial_db]
-  }).then(function(dbPost) {
-    res.json(dbPost);
+    db.Post.findAll({
+      where: query,
+      include: [db.clubSocial_db],
+    }).then(function (dbPost) {
+      res.json(dbPost);
+    });
   });
-});
 
-
-app.get("/api/posts/:id", function(req, res) {
-    
+  app.get("/api/posts/:id", function (req, res) {
     db.Post.findOne({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
-      include: [db.clubSocial]
-    }).then(function(dbPost) {
+      include: [db.clubSocial],
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 
-  app.delete("/api/posts/:id", function(req, res) {
+  app.delete("/api/posts/:id", function (req, res) {
     db.Post.destroy({
       where: {
-        id: req.params.id
-      }
-    }).then(function(dbPost) {
+        id: req.params.id,
+      },
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
-
-
-
-app.put("/api/posts", function(req, res) {
-    db.Post.update(
-      req.body,
-      {
-        where: {
-          id: req.body.id
-        }
-      }).then(function(dbPost) {
+// made a way to update routes
+  app.put("/api/posts", function (req, res) {
+    db.Post.update(req.body, {
+      where: {
+        id: req.body.id,
+      },
+    }).then(function (dbPost) {
       res.json(dbPost);
     });
   });
 };
-
-
-
-
-
-
-
-
-
