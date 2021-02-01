286c9053d76b32e5b82c57128b2a139721895d0d
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-02-01 11:39:20 -0500

    added views route

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,50 +1,58 @@
 const router = require("express").Router();
 
+//Import the model to use its database functions
+const post = require("../models/Post");
+
 //Require API routes
 router.get("/api/posts", (req, res) => {
-    db.Post.findAll().then((allPosts) => {
-      console.log(allPosts);
-      res.json(allPosts);
-    });
+  db.Post.findAll().then((allPosts) => {
+    console.log(allPosts);
+    res.json(allPosts);
   });
-  
-  router.get("/api/posts/:id", (req, res) => {
-    db.Post.findOne({ where: { id: req.params.id } }).then((foundPost) => {
-      console.log(foundPost);
-      res.json(foundPost);
-    });
-  });
-  
-  router.post("/api/posts", (req, res) => {
-    db.Post.create({
-      school: req.body.school,
-      subject: req.body.subject,
-      post: req.body.post,
-      votes: req.body.votes,
-    }).then((newPost) => {
-      console.log(newPost);
-      res.json(newPost);
-    });
-  });
-  
-  router.put("/api/posts/:id", (req, res) => {
-    db.Post.update(req.body, {
-      where: { id: req.params.id },
-    }).then((updatedPost) => {
-      console.log(updatedPost);
-      res.json(updatedPost);
-    });
-  });
-  
-  router.delete("/api/posts/:id", (req, res) => {
-    db.Post.destroy({
-      where: {
-        id: req.params.id,
-      },
-    }).then(() => {
-      console.log("Post deleted.");
-      res.end();
-    });
-  });
+});
 
-  module.exports = router;
+router.get("/api/posts/:id", (req, res) => {
+  db.Post.findOne({ where: { id: req.params.id } }).then((foundPost) => {
+    console.log(foundPost);
+    res.json(foundPost);
+  });
+});
+
+router.post("/api/posts", (req, res) => {
+  db.Post.create({
+    school: req.body.school,
+    subject: req.body.subject,
+    post: req.body.post,
+    votes: req.body.votes,
+  }).then((newPost) => {
+    console.log(newPost);
+    res.json(newPost);
+  });
+});
+
+router.put("/api/posts/:id", (req, res) => {
+  db.Post.update(req.body, {
+    where: { id: req.params.id },
+  }).then((updatedPost) => {
+    console.log(updatedPost);
+    res.json(updatedPost);
+  });
+});
+
+router.delete("/api/posts/:id", (req, res) => {
+  db.Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(() => {
+    console.log("Post deleted.");
+    res.end();
+  });
+});
+
+//Require View Routes
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -20,7 +20,14 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//Import routes and give the server access to them
+const routes = require("./controllers/postController");
+
+app.use(routes);
+
 //Listen on the PORT
 db.sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  app.listen(PORT, () =>
+    console.log(`Listening on PORT http://localhost:${PORT}`)
+  );
 });
