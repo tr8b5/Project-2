b374c285dce3bf3d953106778762572bd126eb75
mya todd <mya.todd428376@gmail.com>
2021-02-02 10:33:08 -0500

    added a ajax to api.js

---
+++ public/api.js
@@ -0,0 +1,35 @@
+function buildQueryURL() {
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://universities-and-colleges.p.rapidapi.com/universities-by-clubsocial");
+
+req.query({
+	"id": "clubsocial"
+});
+
+req.headers({
+	"x-rapidapi-key": "6ccaabf5bfmshd38a0c923082aefp1e6d81jsn91c7de327e84",
+	"x-rapidapi-host": "universities-and-colleges.p.rapidapi.com",
+	"useQueryString": true
+});
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+
+  searchOMDB("Mr. Nobody");
+  searchOMDB("The Little Mermaid");
+  searchOMDB("The Lion King");
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+}
