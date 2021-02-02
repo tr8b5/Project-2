9b9b7db8aa38f5fdfad99dd1eeccb50a5e1fb050
mya todd <mya.todd428376@gmail.com>
2021-02-02 11:46:02 -0500

    added event listener

--- public/api.js
+++ public/api.js
@@ -2,11 +2,7 @@ function buildQueryURL() {
 
 var unirest = require("unirest");
 
-var req = unirest("GET", "https://universities-and-colleges.p.rapidapi.com/universities-by-clubsocial");
-
-req.query({
-	"id": "clubsocial"
-});
+var req = unirest("GET", "https://universities-and-colleges.p.rapidapi.com/universities");
 
 req.headers({
 	"x-rapidapi-key": "6ccaabf5bfmshd38a0c923082aefp1e6d81jsn91c7de327e84",
@@ -14,12 +10,13 @@ req.headers({
 	"useQueryString": true
 });
 
-
+$("school").on("click",  function() {
+    searchColleges($(this).text());
+});
 
 req.end(function (res) {
 	if (res.error) throw new Error(res.error);
 
 	console.log(res.body);
 });
-
 }
